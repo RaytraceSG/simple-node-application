a8f8d7ce86096965927ba5ddf2c1387cf780c926
RaytraceSG <azmi.maniku@gmail.com>
2024-10-10 18:44:47 +0800

    test workflows

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,4 +1,4 @@
-# Create Amazon ECR repository to store Docker image
+# Create Amazon ECR repository to store Docker images
 resource "aws_ecrpublic_repository" "ecr_repo" {
   repository_name = var.ecr_repo_name
 }
