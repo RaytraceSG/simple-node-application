bc00a251354e865782bb6af79bf1af9e931c8293
RaytraceSG <azmi.maniku@gmail.com>
2024-10-10 16:01:59 +0800

    Add docker actions

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -4,11 +4,11 @@ variable "instance_type" {
   default     = "t2.micro"
 }
 
-variable "key_name" {
-  description = "Name of EC2 Key Pair"
-  type        = string
-  default     = "azmi1-keypair-useast1" # Replace with your own key pair name (without .pem extension) that you have downloaded from AWS console previously
-}
+# variable "key_name" {
+#   description = "Name of EC2 Key Pair"
+#   type        = string
+#   default     = "azmi1-keypair-useast1" # Replace with your own key pair name (without .pem extension) that you have downloaded from AWS console previously
+# }
 
 variable "instance_name" {
   description = "EC2 Instance name"
